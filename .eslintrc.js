1ec6b33e907ea68216c79435cd88fb8ed8e1f4ac
Wan Saleh <wansaleh@gmail.com>
2021-05-15 23:10:31 +0800

    Update deps.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,12 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2021,
-    ecmaFeatures: {
-      jsx: true,
-    },
-    sourceType: 'module',
-  },
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  parserOptions: { ecmaVersion: 2020 },
   plugins: ['simple-import-sort'],
   globals: {
     graphql: false,
-    window: true,
+    tw: true,
     document: true,
+    window: true,
     fetch: true,
   },
   rules: {
@@ -33,7 +22,6 @@ module.exports = {
     'import/no-dynamic-require': 0,
     'import/no-extraneous-dependencies': 0,
     'import/prefer-default-export': 0,
-    indent: 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/href-no-hash': 0,
     'jsx-a11y/label-has-for': 0,
@@ -42,7 +30,6 @@ module.exports = {
     'lines-between-class-members': 0,
     'no-empty': 0,
     'no-param-reassign': 0,
-    'no-sparse-arrays': 0,
     'no-underscore-dangle': 0,
     'no-use-before-define': 0,
     'no-useless-concat': 0,
@@ -54,10 +41,6 @@ module.exports = {
     'react/jsx-no-bind': 0,
     'react/jsx-one-expression-per-line': 0,
     'react/jsx-props-no-spreading': 0,
-    // 'react/jsx-wrap-multilines': [
-    //   2,
-    //   { declaration: false, assignment: false }
-    // ],
     'react/no-multi-comp': 0,
     'react/no-danger': 0,
     'react/no-array-index-key': 0,
